82caea9a5c48d1a67aec059d801e006de5dc9edd
Kiss-shot8 <fuchimanico@gmail.com>
2022-08-23 23:34:38 -0300

    cambio en view de mostrar familiar

--- mi_primer_mtv/AppMtv/views.py
+++ mi_primer_mtv/AppMtv/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Context, Template, loader
+from django.template import Context, Template
 from .models import Familiar
 
-def mostrarPrimerFamiliar(request):
+def mostrarFamiliares(request):
     primer_familiar = Familiar(nombre = "Alejandra", edad = 40, fecha_de_nacimiento = 24/3/1982)
 
     primer_familiar.save()
 
-    diccionario = {"Nombre": primer_familiar.nombre, "Edad": primer_familiar.edad, "Fecha_de_nacimiento": primer_familiar.fecha_de_nacimiento}
+    segundo_familiar = Familiar(nombre = "Francisco", edad = 40, fecha_de_nacimiento = 20/10/1982)
 
-    plantilla = loader.get_template('MostrarFamiliar.html')
+    segundo_familiar.save()
 
-    documento = plantilla.render(diccionario)
+    tercer_familiar = Familiar(nombre = "Roque", edad = 60, fecha_de_nacimiento = 14/3/1962)
 
-    return HttpResponse(documento)
+    tercer_familiar.save()
+
+    familiares = [primer_familiar, segundo_familiar, tercer_familiar]
+
+    return render(request, 'MostrarFamiliares.html', {'Familiares': familiares})
