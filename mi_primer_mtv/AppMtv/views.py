e90ff59b9607cd3f9f452a5bf9ae786c3b74d379
Kiss-shot8 <fuchimanico@gmail.com>
2022-08-28 19:35:28 -0300

    Ultimo commit web andando

--- mi_primer_mtv/AppMtv/views.py
+++ mi_primer_mtv/AppMtv/views.py
@@ -3,15 +3,15 @@ from django.template import Context, Template
 from .models import Familiar
 
 def mostrarFamiliares(request):
-    primer_familiar = Familiar(nombre = "Alejandra", edad = 40, fecha_de_nacimiento = 24/3/1982)
+    primer_familiar = Familiar(nombre = "Alejandra", edad = 40, fecha_de_nacimiento = "1982-03-24")
 
     primer_familiar.save()
 
-    segundo_familiar = Familiar(nombre = "Francisco", edad = 40, fecha_de_nacimiento = 20/10/1982)
+    segundo_familiar = Familiar(nombre = "Francisco", edad = 40, fecha_de_nacimiento = "1982-10-20")
 
     segundo_familiar.save()
 
-    tercer_familiar = Familiar(nombre = "Roque", edad = 60, fecha_de_nacimiento = 14/3/1962)
+    tercer_familiar = Familiar(nombre = "Roque", edad = 60, fecha_de_nacimiento = "1963-03-14")
 
     tercer_familiar.save()
 
